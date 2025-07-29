2a4d47d3123400b83d17aca5d544c75afcc121bf
Ricardo Tavares <rtavares@cloudflare.com>
2025-07-29 17:56:46 +0100

    Added OpenNext

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import { withPayload } from '@payloadcms/next/withPayload'
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
+
+initOpenNextCloudflareForDev()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
-  webpack: (webpackConfig) => {
+  webpack: (webpackConfig: any) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
